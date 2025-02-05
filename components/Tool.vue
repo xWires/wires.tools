036e5e5daeda9d3a03dc1f73675b7e7674800edf
xWires <xwires@tangledwires.xyz>
2025-02-05 20:51:23 +0000

    Add rounded corners and scale on hover tool links

--- components/Tool.vue
+++ components/Tool.vue
@@ -7,10 +7,12 @@ const props = defineProps({
 </script>
 
 <template>
-  <NuxtLink :to="url">
-    <div class="glowing-border" style="width: 100%; height: 100%;">
-      <p class="text-lg">{{ title }}</p>
-      <p>{{ description }}</p>
-    </div>
-  </NuxtLink>
+  <div class="glowing-border transition duration-150 ease-in-out hover:scale-105" style="width: 100%; height: 100%; border-radius: 15px; padding: 10px">
+    <NuxtLink :to="url">
+      <div>
+        <p class="text-lg">{{ title }}</p>
+        <p>{{ description }}</p>
+      </div>
+    </NuxtLink>
+  </div>
 </template>
