fb5b1d87c8364b203bdfb5150549aea26a56c2f3
xWires <68852080+xWires@users.noreply.github.com>
2025-02-06 13:40:18 +0000

    XML Validator (#7)

--- pages/index.vue
+++ pages/index.vue
@@ -3,5 +3,6 @@
     <Tool title="Base64 Encoder/Decoder" description="Encode or decode Base64" url="/tools/base64" />
     <Tool title="JSON Validator" description="Make sure your JSON is valid" url="/tools/jsonValidator" />
     <Tool title="YAML Validator" description="Make sure your YAML is valid" url="/tools/yamlValidator" />
+    <Tool title="XML Validator" description="Make sure your XML is valid" url="/tools/xmlValidator" />
   </div>
 </template>
---
+++ pages/tools/xmlValidator.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { useTemplateRef } from "vue";
+import { XMLParser } from "fast-xml-parser";
+
+const input = useTemplateRef("input");
+const errorContainer = useTemplateRef("error-container");
+const successContainer = useTemplateRef("success-container");
+const error = useTemplateRef("error");
+
+const parser = new XMLParser();
+
+function validateXml() {
+  successContainer.value.style.display = "none";
+  errorContainer.value.style.display = "none";
+  try {
+    parser.parse(input.value.value, { allowBooleanAttributes: true });
+    successContainer.value.style.display = "block";
+  }
+  catch (e) {
+    errorContainer.value.style.display = "block";
+    error.value.innerHTML = e.toString();
+  }
+}
+</script>
+
+<template>
+  <div>
+    <p style="font-size: 30px">XML Validator</p>
+    <div style="margin-top: 15px; margin-bottom: 15px;">
+      <p style="font-size: 25px; margin-bottom: 15px">Input</p>
+      <textarea ref="input" class="glowing-border-green" style="width: 100%; height: 150px"></textarea>
+    </div>
+    <button @click="validateXml" class="glowing-border-green">Validate</button>
+    <div ref="error-container" class="glowing-border-red" style="margin-top: 50px; display: none">
+      <p style="font-size: 25px;">Invalid XML</p>
+      <br>
+      <pre><code ref="error"></code></pre>
+    </div>
+    <div ref="success-container" class="glowing-border-green" style="margin-top: 50px; display: none">
+      <p style="font-size: 25px;">Valid XML!</p>
+    </div>
+  </div>
+</template>
