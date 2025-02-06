33d02cd310c099f950ced5b0de807b63307c8a2a
xWires <68852080+xWires@users.noreply.github.com>
2025-02-06 12:55:09 +0000

    JSON Validator (#5)

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -3,7 +3,7 @@ import "~/assets/css/header.css"
 </script>
 
 <template>
-  <div class="glowing-border" id="header-container">
+  <div class="glowing-border-green" id="header-container">
     <a href="/"><NuxtImg src="/xwires.png" width="75px" style="padding: 15px;" /></a>
     <a href="/" class="neat-link" style="padding-left: 15px; font-size: 40px;">Tools</a>
     <a href="https://github.com/xWires/wires.tools" style="margin-left: auto; margin-right: 15px;"><i class="fa-brands fa-github" style="color: white; font-size: 40px;"></i></a>
