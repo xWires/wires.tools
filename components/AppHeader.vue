723a6cdb1477de803a5230f4991f2d2c2e507c51
xWires <xwires@tangledwires.xyz>
2025-02-07 14:10:30 +0000

    Add ko-fi link

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -6,6 +6,7 @@ import "~/assets/css/header.css"
   <div class="glowing-border-green" id="header-container">
     <a href="/"><NuxtImg src="/xwires.png" width="75px" style="padding: 15px;" /></a>
     <a href="/" class="neat-link" style="padding-left: 15px; font-size: 40px;">Tools</a>
-    <a href="https://github.com/xWires/wires.tools" style="margin-left: auto; margin-right: 15px;"><i class="fa-brands fa-github" style="color: white; font-size: 40px;"></i></a>
+    <a href="https://ko-fi.com/tangledwires" style="margin-left: auto; margin-right: 15px"><img src="/kofi_symbol.svg" width="45px" alt="Ko-Fi Logo"></a>
+    <a href="https://github.com/xWires/wires.tools" style="margin-right: 15px;"><i class="fa-brands fa-github" style="color: white; font-size: 40px;"></i></a>
   </div>
 </template>
