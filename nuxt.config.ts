5c9ebe81a1a494eac1886d8ea4b45055004101c0
xWires <xwires@tangledwires.xyz>
2025-02-10 14:21:41 +0000

    Add nuxt-monaco-editor

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
-  modules: ['@nuxt/image', '@nuxtjs/plausible'],
+  modules: ['@nuxt/image', '@nuxtjs/plausible', 'nuxt-monaco-editor'],
   app: {
     head: {
       script: [
