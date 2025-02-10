194244617cd40f3c29414e33883f87f6eb184277
xWires <xwires@tangledwires.xyz>
2025-02-10 18:08:10 +0000

    Combine footer.css into AppFooter.vue

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -7,3 +7,14 @@ import "~/assets/css/footer.css"
     <span class="footer-text">&copy; Copyright <a class="footer-text" href="https://tangledwires.xyz/">TangledWires</a> 2025</span><a class="footer-text" href="/legal/privacy" style="margin-left: auto">Privacy Policy</a>
   </div>
 </template>
+
+<style scoped>
+.footer {
+  padding: 15px;
+}
+
+.footer-text {
+  color: darkgray;
+  font-family: Arial, Helvetica, sans-serif;
+}
+</style>
