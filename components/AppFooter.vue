52d6f2a2286e1564e1a53a12e9375e9dfdccfc21
xWires <xwires@tangledwires.xyz>
2025-02-28 10:21:01 +0000

    Remove footer.css (unused)

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,7 +1,3 @@
-<script>
-import "~/assets/css/footer.css"
-</script>
-
 <template>
   <div class="footer" style="display: flex; background-color: #0b0b0b;">
     <span class="footer-text">&copy; Copyright <a class="footer-text" href="https://tangledwires.xyz/">TangledWires</a> 2025</span><a class="footer-text" href="/legal/privacy" style="margin-left: auto">Privacy Policy</a>
