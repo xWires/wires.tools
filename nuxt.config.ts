3637636934ea8dcfa1ca0e43caca89c0b627e9d4
xWires <xwires@tangledwires.xyz>
2025-05-30 20:33:29 +0100

    Use npm version of wireui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   extends: [
-      "github:xWires/wireui"
+      "@xwires/wireui"
   ],
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
