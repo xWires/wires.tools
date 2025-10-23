5a8a7b74bfd478e1566be165eb8c554d7d11e80e
xWires <xwires@tangledwires.xyz>
2025-10-23 17:34:42 +0100

    Escape chars in csv to html table converter

--- pages/tools/csvHtmlTable.vue
+++ pages/tools/csvHtmlTable.vue
@@ -1,6 +1,7 @@
 <script setup>
 import {ref} from "vue";
 import Papa from 'papaparse';
+import {escapeForHtml} from "#shared/utils/escapeForHtml.ts";
 
 useSeoMeta({
   title: 'CSV to HTML Table - TangledWires Tools',
@@ -12,6 +13,16 @@ const input = useTemplateRef("input");
 const output = ref("");
 
 const errors = ref("");
+
+const shouldEscape = ref(true);
+
+function maybeEscape(c) {
+  if (shouldEscape.value) {
+    return escapeForHtml(c);
+  } else {
+    return c;
+  }
+}
 
 function convert() {
   const records = Papa.parse(input.value.value);
@@ -30,12 +41,12 @@ function convert() {
   output.value = `<table>
   <thead>
     <tr>
-      ${columnNames.map(columnName => `<th>${columnName}</th>`).join("\n      ")}
+      ${columnNames.map(columnName => `<th>${maybeEscape(columnName)}</th>`).join("\n      ")}
     </tr>
   </thead>
   <tbody>
     ${data.map(row => `<tr>
-      ${row.map(col => `<td>${col}</td>`).join("\n      ")}
+      ${row.map(col => `<td>${maybeEscape(col)}</td>`).join("\n      ")}
     </tr>`).join("\n    ")}
   </tbody>
 </table>`;
@@ -50,6 +61,8 @@ function convert() {
       <textarea ref="input" class="glowing-border-green" style="width: 100%; height: 500px"></textarea>
     </div>
     <BaseButton @click="convert" style="margin-top: 20px">Convert</BaseButton>
+    <input v-model="shouldEscape" type="checkbox" style="margin-left: 15px" checked />
+    <span style="margin-left: 5px">Escape characters for HTML</span>
     <div style="margin-top: 15px; margin-bottom: 15px;">
       <p style="font-size: 25px; margin-bottom: 15px">Output</p>
       <div v-if="errors" class="glowing-border-red" style="margin-bottom: 20px;">
---
+++ shared/utils/escapeForHtml.ts
@@ -0,0 +1,7 @@
+export function escapeForHtml(str: string) {
+    return str.replaceAll("&", "&amp;")
+        .replaceAll("<", "&lt;")
+        .replaceAll(">", "&gt;")
+        .replaceAll("\"", "&quot;")
+        .replaceAll("'", "&#039;");
+}
